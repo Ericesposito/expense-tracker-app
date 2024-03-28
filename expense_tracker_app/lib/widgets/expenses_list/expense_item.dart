e37dfb3afb494969d5c933c85041a9d3965da5f7
Ericesposito <eespo93@optonline.net>
2024-03-28 19:03:15 -0400

    Started adding ThemeData + global variable for simplified theming around application. textTheme, cardTheme, Theme.of(context), as well as other concepts solidifed and implemented. Made Expense widgets Dismissible, which allows the user to delete their expenses, as well as undoing a deletion.

--- expense_tracker_app/lib/widgets/expenses_list/expense_item.dart
+++ expense_tracker_app/lib/widgets/expenses_list/expense_item.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker_app/models/expense.dart';
-import 'package:flutter/widgets.dart';
 
 class ExpenseItem extends StatelessWidget {
   const ExpenseItem(this.expense, {super.key});
@@ -17,8 +16,12 @@ class ExpenseItem extends StatelessWidget {
           vertical: 16.0,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
             const SizedBox(height: 4),
             Row(
               children: [
--- expense_tracker_app/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker_app/lib/widgets/expenses_list/expenses_list.dart
@@ -1,18 +1,33 @@
+import 'package:expense_tracker_app/main.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker_app/models/expense.dart';
 import 'package:expense_tracker_app/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(expenses[index]),
+      ),
     );
   }
 }
